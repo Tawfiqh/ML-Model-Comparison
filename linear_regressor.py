8d2dfd86eda51a1bdc5c62324043d26e81f3bdf0
Ben Pashley <ben.pashley@gmail.com>
2021-07-26 21:13:34 +0100

    linear regression implementation

--- linear_regressor.py
+++ linear_regressor.py
@@ -1,12 +1,17 @@
 from sklearn import linear_model
-
 class LinearRegressor:
     def __init__(self) -> None:
         self.model = linear_model.LinearRegression()
 
-    def score_all():
-        pass
+    def fit(self, dataset):
+        X = dataset[0]
+        y = dataset[1]
+        self.model.fit(X, y)
 
-    def fit():
-        pass
+    def score_all(self, train, test, val):
+        score_train = self.model.score(train[0], train[1])
+        score_test = self.model.score(train[0], train[1])
+        score_val = self.model.score(train[0], train[1])
+        return score_train, score_test, score_val
 
+# {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'normalize': False, 'positive': False}
