2e678448da5aaea13410de55d78da86d9b3e754d
unknown <kautarnahli@hotmail.es>
2021-07-26 22:08:13 +0100

    grid search

--- svm.py
+++ svm.py
@@ -1,20 +1,35 @@
+#%%
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn import preprocessing
+from sklearn import datasets
+import numpy as np
+dataset = datasets.load_boston(return_X_y=True)
 class Svc:
-    from sklearn.svm import SVC
+    
     def __init__(self) -> None:
         pass
+
 
     def fit(self, dataset):
         #C : float, default=1.0
         #kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}, default=’rbf’
         X = dataset[0]
         y = dataset[1]
-        all_scores = []
-        for i in ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']:
-                svc = SVC(kernel= i, gamma='auto').fit(X, y) # gamma='scale'
-                svc.fit(X,y)
-                all_scores.append(svc.score(X, y, sample_weight=None))
+        param_grid = {
+            'C': np.logspace(-3, 2, 6), 
+            'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+            'gamma': np.logspace(-3, 2, 6)
+            }
+        clf = GridSearchCV(SVC(), param_grid,cv=5,scoring='accuracy')
+        clf.fit(X, y)
+        print(clf.best_params_)
 
-
+        
     def score_all(self, train, test, val):
         pass
-        
+
+a = Svc()
+a.fit(dataset)
+
+# %%
