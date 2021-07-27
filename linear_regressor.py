653aba57420c1c6b004eba58ddb02f400ece1c46
Ben Pashley <ben.pashley@gmail.com>
2021-07-27 19:45:28 +0100

    update to LR model

--- linear_regressor.py
+++ linear_regressor.py
@@ -1,17 +1,45 @@
+# %%
 from sklearn import linear_model
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+
 class LinearRegressor:
     def __init__(self) -> None:
-        self.model = linear_model.LinearRegression(fit_intercept=))
+        self.model = linear_model.LinearRegression()
 
     def fit(self, dataset):
+
         X = dataset[0]
         y = dataset[1]
+
+        # defining parameter range 
+        param_grid = {'fit_intercept':[True,False], 
+                        'normalize':[True,False], 
+                        'copy_X':[True, False]}
+  
+        self.model = GridSearchCV(self.model, param_grid)
+  
         self.model.fit(X, y)
+        
+        # # print best parameter after tuning 
+        # print(self.model.get_params().keys()) 
+
+        # # print best parameter before tuning 
+        # # print(self.model.best_params_) 
+
+        # # print best parameter after tuning 
+        # print(self.grid_model.best_params_) 
+
+        # print (f'r2 / variance : {self.grid_model.best_score_}')
+        
+        # print('Residual sum of squares: %.2f'
+        #       % np.mean((self.grid_model.predict(X) - y) ** 2))
 
     def score_all(self, train, test, val):
         score_train = self.model.score(*train)
-        score_test = self.model.score(*test])
+        score_test = self.model.score(*test)
         score_val = self.model.score(*val)
         return score_train, score_test, score_val
 
-# {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'normalize': False, 'positive': False}
+
+# %%
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from k_nearest import KNearest
 from linear_regressor import LinearRegressor
 from get_data import get_boston_train_test_val_datasets
 
-models = {"K-Nearest": KNearest(), "LinearRegressor": LinearRegressor()}
+models = {"LinearRegressor": LinearRegressor()}
 
 
 data_set = get_boston_train_test_val_datasets()
@@ -19,14 +19,13 @@ for model_name in models.keys():
     all_model_results.append(model_results)
 
 
-df = pd.DataFrame(
-    all_results,
-    # columns=["model_name", "training_score", "testing_score", "validation_score"],
-)
-# pd.options.display.float_format = "{:,.4f}".format
+# df = pd.DataFrame(all_results,
+#     # columns=["model_name", "training_score", "testing_score", "validation_score"],
+# )
+# # pd.options.display.float_format = "{:,.4f}".format
 
-print(df)
+# print(df)
 
-# best_result = df[df["validation_score"] == df["validation_score"].max()]
-# print(best_result["model_name"].head(1).item())
+# # best_result = df[df["validation_score"] == df["validation_score"].max()]
+# # print(best_result["model_name"].head(1).item())
 
