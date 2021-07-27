7ce7bbd02c4f4540841c0c4782b0fdafa951a011
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2021-07-27 20:09:10 +0100

    Added timing to DF showing model comparison

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from linear_regressor import LinearRegressor
 from svm import Svm_svr
 from get_data import get_boston_train_test_val_datasets
 import pandas as pd
+from time import perf_counter
+
 
 models = {
     "K-Nearest": KNearest(),
@@ -17,21 +19,35 @@ all_model_results = []
 
 for model_name in models.keys():
     model = models[model_name]
+    time_start = perf_counter()
 
     # FIT
     model.fit(data_set["train"])
+
+    time_finished_fit = perf_counter()
+    fit_time = time_finished_fit - time_start
 
     # SCORE ALL
     model_results = model.score_all(
         data_set["train"], data_set["test"], data_set["val"]
     )
+    time_finished_scoring = perf_counter()
+    scoring_time = time_finished_fit - time_start
+
     # print(f"model_results:{model_results}")
     if model_results:
-        all_model_results.append([model_name, *model_results])
+        all_model_results.append([model_name, fit_time, scoring_time, *model_results])
 
 df = pd.DataFrame(
     all_model_results,
-    columns=["model_name", "training_score", "testing_score", "validation_score"],
+    columns=[
+        "model_name",
+        "fit_time",
+        "scoring_time",
+        "training_score",
+        "testing_score",
+        "validation_score",
+    ],
 )
 pd.options.display.float_format = "{:,.4f}".format
 
