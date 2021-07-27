6b31a8055ccb0b0123110f68aeca905732b40e9c
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2021-07-27 19:58:45 +0100

    Removed a comment

--- linear_regressor.py
+++ linear_regressor.py
@@ -1,7 +1,6 @@
-# %%
 from sklearn import linear_model
 from sklearn.model_selection import GridSearchCV
-import numpy as np
+
 
 class LinearRegressor:
     def __init__(self) -> None:
@@ -12,26 +11,28 @@ class LinearRegressor:
         X = dataset[0]
         y = dataset[1]
 
-        # defining parameter range 
-        param_grid = {'fit_intercept':[True,False], 
-                        'normalize':[True,False], 
-                        'copy_X':[True, False]}
-  
+        # defining parameter range
+        param_grid = {
+            "fit_intercept": [True, False],
+            "normalize": [True, False],
+            "copy_X": [True, False],
+        }
+
         self.model = GridSearchCV(self.model, param_grid)
-  
+
         self.model.fit(X, y)
-        
-        # # print best parameter after tuning 
-        # print(self.model.get_params().keys()) 
 
-        # # print best parameter before tuning 
-        # # print(self.model.best_params_) 
+        # # print best parameter after tuning
+        # print(self.model.get_params().keys())
 
-        # # print best parameter after tuning 
-        # print(self.grid_model.best_params_) 
+        # # print best parameter before tuning
+        # # print(self.model.best_params_)
+
+        # # print best parameter after tuning
+        # print(self.grid_model.best_params_)
 
         # print (f'r2 / variance : {self.grid_model.best_score_}')
-        
+
         # print('Residual sum of squares: %.2f'
         #       % np.mean((self.grid_model.predict(X) - y) ** 2))
 
